9bf3a767715f9542a9309f525f22f6c905a650b0
Wetcher <13554906+phooeyjay@users.noreply.github.com>
2023-05-18 13:06:31 +0800

    Install Konva, prepare tsconfig.json & .ts files

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,13 @@
-'use strict';
+import Konva from 'konva';
 
-export class Game {
-    private _canvas: HTMLElement = undefined;
-    private _timestamp: number = 0;
-    private _events: Event[] = [];
+export default class KonvaGame {
+    private app: Konva.Stage;
 
-    init = (renderer: HTMLElement) => {
-        this._canvas = renderer instanceof HTMLCanvasElement && renderer || (() => { throw new TypeError('Element provided is not of type [Canvas]') })();
-
-        ['keydown', 'mousemove'].forEach(e => { addEventListener(e, event => { this._events.push(event) }) });
-        this._canvas.addEventListener('click', event => { this._events.push(event) });
-
-        requestAnimationFrame(this._gameLoop);
-    }
-
-    private _gameLoop = (ts: number) => {
-        this._input();
-
-        this._update(ts - this._timestamp);
-        this._timestamp = ts;
-
-        this._render();
-
-        requestAnimationFrame(this._gameLoop);
-    }
-
-    private _input = () => {
-        while (this._events.length !== 0) {
-            console.log(this._events.shift());
-        }
-        //TODO
-    }
-
-    private _update = (delay: number) => {
-        //TODO
-    }
-
-    private _render = () => {
-        //TODO
+    constructor (container: HTMLDivElement) {
+        this.app = new Konva.Stage({ container: container.id, width: container.clientWidth, height: container.clientHeight });
+        container.addEventListener('resize', () => {
+            this.app.width(container.clientWidth);
+            this.app.height(container.clientHeight);
+        });
     }
 }
